de9ec309a5e09a4bb4f1ef085dbf48e7f60b14f3
Gustaf Wall <wallgu@utb.fuv.se>
2016-05-07 22:52:26 +0200

    Fixed merging mabey

--- ViveSandboxProj/Assets/Scripts/Editor/MergeableObjectEditor.cs
+++ ViveSandboxProj/Assets/Scripts/Editor/MergeableObjectEditor.cs
@@ -1,80 +1,38 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using System.Collections;
 
-public class Child : MonoBehaviour
+[CustomEditor(typeof(MergeableObject))]
+public class MergeableObjectEditor : Editor
 {
-
-    public Transform parentTransform;
-
-    // If true, will attempt to scale the child accurately as the parent scales
-    // Will not be accurate if starting rotations are different or irregular
-    // Experimental
-    public bool attemptChildScale = false;
-
-    Vector3 startParentPosition;
-    Quaternion startParentRotationQ;
-    Vector3 startParentScale;
-
-    Vector3 startChildPosition;
-    Quaternion startChildRotationQ;
-    Vector3 startChildScale;
-
-    Matrix4x4 parentMatrix;
-
-    void Start()
+    public override void OnInspectorGUI()
     {
+        DrawDefaultInspector();
 
-        startParentPosition = parentTransform.position;
-        startParentRotationQ = parentTransform.rotation;
-        startParentScale = parentTransform.lossyScale;
-
-        startChildPosition = transform.position;
-        startChildRotationQ = transform.rotation;
-        startChildScale = transform.lossyScale;
-
-        // Keeps child position from being modified at the start by the parent's initial transform
-        startChildPosition = DivideVectors(Quaternion.Inverse   (parentTransform.rotation) * (startChildPosition - startParentPosition), startParentScale);
-    }
-
-    void Update()
-    {
-
-        parentMatrix = Matrix4x4.TRS(parentTransform.position, parentTransform.rotation, parentTransform.lossyScale);
-
-        transform.position = parentMatrix.MultiplyPoint3x4(startChildPosition);
-
-        transform.rotation = (parentTransform.rotation * Quaternion.Inverse(startParentRotationQ)) * startChildRotationQ;
-
-        // Incorrect scale code; it scales the child locally not gloabally; Might work in some cases, but will be inaccurate in others
-        if (attemptChildScale)
+        var obj = (MergeableObject)target;
+        switch (obj.MergableObjectType)
         {
-            transform.localScale = Vector3.Scale(startChildScale, DivideVectors(parentTransform.lossyScale, startParentScale));
+            case MergeableObject.MergableObjectTypes.SpecTag:
+                obj.MergeTag = EditorGUILayout.TextField("Merge Tag", obj.MergeTag);
+                break;
+            case MergeableObject.MergableObjectTypes.SpecObject:
+                obj.MergeObject = (Rigidbody)EditorGUILayout.ObjectField("Merge Object", obj.MergeObject, typeof(Rigidbody), true);
+                break;
         }
 
-        // Scale code 2; I was working on to scale the child globally through it's local scale, but turned out to be impossible using localScale
-        /*
-        Vector3 modVec;
+        if (obj.UseMergeRail)
+        {
+            obj.MergeRailStart = (Transform)EditorGUILayout.ObjectField("Merge Rail Start", obj.MergeRailStart, typeof(Transform), true);
+            obj.MergeTarget = (Transform)EditorGUILayout.ObjectField("Merge Rail End", obj.MergeTarget, typeof(Transform), true);
+            obj.MergeRailThresholdDistance = EditorGUILayout.FloatField("Merge Rail Threshold Distance", obj.MergeRailThresholdDistance);
+            obj.MergeRailThresholdAngle = EditorGUILayout.FloatField("Merge Rail Threshold Angle", obj.MergeRailThresholdAngle);
+        }
+        else
+        {
+            obj.MergeTarget = (Transform)EditorGUILayout.ObjectField("Merge Target", obj.MergeTarget, typeof(Transform), true);
+        }
 
-        float angleX = Mathf.Abs(Vector3.Angle(transform.right, parentTransform.right));
-
-        modVec.x = Mathf.Abs(angleX - 90) / 90;
-
-        float angleY = Mathf.Abs(Vector3.Angle(transform.up, parentTransform.up));
-
-        modVec.y = Mathf.Abs(angleY - 90) / 90;
-
-        float angleZ = Mathf.Abs(Vector3.Angle(transform.forward, parentTransform.forward));
-
-        modVec.z = Mathf.Abs(angleZ - 90) / 90;
-
-        transform.localScale = Vector3.Scale(startChildScale, Vector3.Scale(DivideVectors(parentTransform.lossyScale, startParentScale), modVec));
-        */
-    }
-
-    Vector3 DivideVectors(Vector3 num, Vector3 den)
-    {
-
-        return new Vector3(num.x / den.x, num.y / den.y, num.z / den.z);
-
+        if (GUI.changed)
+            EditorUtility.SetDirty(target);
     }
 }
